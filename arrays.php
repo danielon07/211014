9bfbfae82b8216448ee01469d1582100a3f0e6d5
Danibalv <danielfozaneldi@gmail.com>
2021-10-14 09:46:30 +0200

    commit github 2

--- arrays.php
+++ arrays.php
@@ -1,1 +1,29 @@
-php
+
+
+     $cosas['a']=10;
+     $cosas['b']=11;
+     $cosas['c']=12;
+     $cosas['d']=13;
+     $cosas['f']=14;
+     
+     foreach($cosas as  $clave=>$valor) {
+         echo "<tr>";
+         echo "<td>";
+         echo $clave;
+         echo "</td>";
+
+         echo "<td>";
+         echo $valor;
+         echo "</td>";
+         echo "</tr>";
+     
+
+
+     }
+
+     foreach($cosas $clave => $valor){
+        foreach($cosas as $clave =>$valor){
+           echo $valor."-".$clave2."-".$valor2;
+          }
+        }
+     
